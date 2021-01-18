b07823a5c505c2bb0100022c0acfb21612ffa6d5
Jack Betteridge <J.Betteridge@imperial.ac.uk>
2021-01-18 23:31:24 +0000

    Updated install and utility scripts

--- install/firedrake_activate.sh
+++ install/firedrake_activate.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 # LOAD MODULES
+module purge
+module load python/3.6.5
+module load gnu_comp/10.2.0
+module load cmake/3.18.1
+module load bison/3.4.1
+
+unset LD_LIBRARY_PATH
 
 export VENV_NAME=firedrake  # Or whatever you named the venv
-export DATA=$HOME/data
+#export DATA=$HOME/data
+export DATA=/cosma5/data/do008/dc-bett2
 export INSTALL_DIR=/tmp/firedrake
-
-export MPICC=`which mpicc`
-export MPICXX=`which mpicxx`
-export MPIF90=`which mpif90`
 
 # Set main to be working directory
 # Create this in /tmp so we don't have issues with the lustre filesystem
@@ -16,8 +20,11 @@ mkdir -p $INSTALL_DIR
 cd $INSTALL_DIR
 
 # Set the PyOP2 compiler to ???
-#export PYOP2_BACKEND_COMPILER=cc
+# export PYOP2_BACKEND_COMPILER=$VIRTUAL_ENV/bin/mpicc
 
 tar -xzf $DATA/bin/$VENV_NAME.tar.gz -C $INSTALL_DIR
 
 source $INSTALL_DIR/$VENV_NAME/bin/activate
+
+# Return to original directory
+cd -
--- install/update_firedrake_cache.sh
+++ install/update_firedrake_cache.sh
@@ -2,4 +2,4 @@
 
 # INSTALL_DIR should be /tmp/firedrake
 cd $INSTALL_DIR
-tar -czvf $DATA/bin/cache_$VENV_NAME.tar.gz cache_$VENV_NAME
+tar -czvf $DATA/bin/cache_$VENV_NAME.tar.gz .cache_$VENV_NAME
