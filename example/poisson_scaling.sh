56aff9e88f8a6df127712bc4843848e0f10cdfee
Jack Betteridge <dc-bett2@b101.pri.cosma7.alces.network>
2021-01-18 23:29:35 +0000

    Updated example submission scripts

--- example/poisson_scaling.sh
+++ example/poisson_scaling.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-BATCH_NAME=poisson_test
+BATCH_NAME=poisson_telescope
 SOLVER_PARAMS="MG F-cycle PatchPC telescope"
 RESULTS_DIR=results/${BATCH_NAME}
 mkdir -p ${RESULTS_DIR}
 
 SPACING=1
-for NODES in 1 2 4 #8 16
+for NODES in 1 2 4 8 16
     do
     sbatch -N ${NODES} \
         -J ${BATCH_NAME} \
