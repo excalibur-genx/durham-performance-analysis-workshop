df975fa55c56e43cbcc03f470f05bac1f5bc95dd
Jack Betteridge <J.Betteridge@imperial.ac.uk>
2021-02-15 14:11:25 +0000

    Script to install Python 3.8

---
+++ install/install_python.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+module purge
+module load gnu_comp/10.2.0
+module load cmake/3.18.1
+module load bison/3.4.1
+module load openmpi/4.0.5
+module load ucx/1.8.1
+
+# Installer variables
+export DATA=$HOME/data
+
+# Create bin folder and link helper scripts
+mkdir -p $DATA/bin
+ln -s $PWD/firedrake_activate.sh $DATA/bin/
+
+export TEMP_PATH=/tmp/firedrake
+mkdir -p $TEMP_PATH
+cd $TEMP_PATH
+
+# LibFFI
+git clone https://github.com/libffi/libffi.git
+cd libffi
+./autogen.sh
+./configure --prefix=$TEMP_PATH/libffi/libffi --disable-docs
+make -j32
+make -j32 install
+cd ..
+
+# OpenSSL
+git clone https://github.com/openssl/openssl.git
+cd openssl
+git checkout -b OpenSSL_1_1_1 tags/OpenSSL_1_1_1
+mkdir openssl
+./config --prefix=$TEMP_PATH/openssl/openssl
+make -j32
+make -j32 install
+cd ..
+
+# Python
+git clone https://github.com/python/cpython.git
+mkdir py38
+cd cpython
+git checkout -b v3.8.7 tags/v3.8.7
+
+PY_LDFLAGS="$LDFLAGS -Wl,-rpath=$TEMP_PATH/openssl/openssl/lib -L$TEMP_PATH/openssl/openssl/lib"
+PY_LDFLAGS="$PY_LDFLAGS -Wl,-rpath=$TEMP_PATH/libffi/libffi/lib64 -L$TEMP_PATH/libffi/libffi/lib64"
+PY_CFLAGS="-I$TEMP_PATH/openssl/openssl/include -I$TEMP_PATH/libffi/libffi/include"
+PY_CPPFLAGS="-I$TEMP_PATH/openssl/openssl/include -I$TEMP_PATH/libffi/libffi/include"
+./configure --prefix=$TEMP_PATH/py38 \
+    --with-ensurepip --with-openssl=$TEMP_PATH/openssl \
+    --enable-shared --enable-profiling --enable-optimizations \
+    LDFLAGS="$PY_LDFLAGS" CFLAGS="$PY_CFLAGS" CPPFLAGS="$PY_CPPFLAGS"
+make -j32
+make -j32 install
+cd ..
