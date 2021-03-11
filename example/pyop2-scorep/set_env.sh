9011905d3372ad89435b5984c94d6dd2b61912b6
Connor Ward <c.ward20@imperial.ac.uk>
2021-03-11 17:26:38 +0000

    This just about works.
    I can't see the kernel invocation so I'm going to fiddle with the filter.

---
+++ example/pyop2-scorep/set_env.sh
@@ -0,0 +1,33 @@
+export DATA=/cosma5/data/durham/dc-ward1
+export INSTALL_DIR=/tmp/firedrake
+export VENV_NAME=firedrake
+
+# Load modules
+module purge
+module load gnu_comp/10.2.0
+module load cmake/3.18.1
+module load bison/3.4.1
+module load openmpi/4.0.5
+module load ucx/1.8.1
+
+# These are set in the activate script but somehow not exported
+export OMPI_MCA_btl_tcp_if_include=p1p2
+export UCX_NET_DEVICES=mlx5_1:1
+
+# 256MB
+export SCOREP_TOTAL_MEMORY=262144000
+#export SCOREP_PROFILING_MAX_CALLPATH_DEPTH=45
+export SCOREP_PROFILING_MAX_CALLPATH_DEPTH=30
+export SCOREP_FILTERING_FILE="scorep.filter"
+export SCOREP_EXPERIMENT_DIRECTORY="results"
+
+export PYOP2_BACKEND_COMPILER="scorep-gcc"
+
+source $INSTALL_DIR/$VENV_NAME/bin/activate
+
+PATH=$PATH:$INSTALL_DIR/scorep_build/bin
+
+# $CC and $CXX interfere with PYOP2
+unset CC
+unset CXX
+export OMP_NUM_THREADS=1
