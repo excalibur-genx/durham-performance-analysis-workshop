16b1d34c88f20b44349052d222c6bf2fdd1594d6
Connor Ward <c.ward20@imperial.ac.uk>
2021-03-11 17:26:38 +0000

    This just about works.
    I can't see the kernel invocation so I'm going to fiddle with the filter.

---
+++ example/pyop2-scorep/rhs.py
@@ -0,0 +1,26 @@
+import time
+
+from firedrake import *
+from firedrake.assemble import create_assembly_callable
+
+
+MESH_SIZE = 1000
+N_REPEATS = 1000
+
+
+mesh = UnitSquareMesh(MESH_SIZE, MESH_SIZE)
+V = FunctionSpace(mesh, "CG", 3)
+v = TestFunction(V)
+L = v * dx
+
+PETSc.Sys.Print(f"Total DoF: {V.dim()}")
+
+out = Function(V)
+callable_ = create_assembly_callable(L, tensor=out)
+
+start = time.time()
+for _ in range(N_REPEATS):
+    callable_()
+end = time.time()
+
+PETSc.Sys.Print(f"Time per assemble (us): {(end-start) / N_REPEATS * 1e6}")
